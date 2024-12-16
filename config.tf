2c6b181a978a0a13d8095b863bb4a79a2b56448f
Rohith Jayawardene <gambol99@gmail.com>
2024-12-16 17:52:07 +0000

    chore: changing the prefix

--- config.tf
+++ config.tf
@@ -5,7 +5,7 @@ module "config_rule_groups" {
   source   = "appvia/stackset/aws"
   version  = "0.1.6"
 
-  name                 = format("%s-%s", var.config.stackset_name_prefix, lower(each.key))
+  name                 = format("%s%s", var.config.stackset_name_prefix, lower(each.key))
   description          = format("Used to configure and distribute the AWS Config rules for %s", each.key)
   enabled_regions      = try(each.value.enabled_regions, null)
   exclude_accounts     = each.value.exclude_accounts
