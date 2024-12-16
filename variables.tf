4b256938d7bf669b799bb6383b0f2a20425ad54f
Rohith Jayawardene <gambol99@gmail.com>
2024-12-16 17:16:15 +0000

    fix: resolving the typo on the field name

--- variables.tf
+++ variables.tf
@@ -11,7 +11,7 @@ variable "config" {
       # List of regions to enable these rules
       enabled_regions = optional(list(string), null)
       # A map of rules contained and deployed from this rule group
-      excluded_accounts = optional(list(string), null)
+      exclude_accounts = optional(list(string), null)
       # The list of accounts to exclude from the organization managed rule
       rules = map(object({
         # The description of the organization managed rules
