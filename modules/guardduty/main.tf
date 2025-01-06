22845005c48d965001c35b3ad73b74af64b00957
Priyesh Mistry <priyeshmis@gmail.com>
2025-01-06 14:37:41 +0000

    chore: add lifecycle ignore

--- modules/guardduty/main.tf
+++ modules/guardduty/main.tf
@@ -30,7 +30,7 @@ resource "aws_guardduty_organization_configuration_feature" "this" {
   dynamic "additional_configuration" {
     for_each = try(each.value.additional_configuration, {})
     content {
-      auto_enable = additional_configuration.value
+      auto_enable = additional_configuration.value.auto_enable
       name        = additional_configuration.key
     }
   }
