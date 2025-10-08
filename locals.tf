2c0b01ebfc085015e949230946890c1dd219f52f
Warren Ayling <warren.ayling@appvia.io>
2025-10-08 14:54:03 +0100

    fix(SA-603): formatting

--- locals.tf
+++ locals.tf
@@ -4,4 +4,7 @@ locals {
   region = data.aws_region.current.name
   ## Tag applied to all resources
   tags = merge(var.tags, {})
+
+  # using Organization - resolve the mgmt account id (it's only ever the one account in the Management OU)
+  mgmt_account_id = data.aws_organizations_organization.this.master_account_id
 }
