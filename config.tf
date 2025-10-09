c8ff2f70bb07656da887231a2b1a1ccaf679142c
Warren Ayling <warren.ayling@appvia.io>
2025-10-09 14:12:06 +0100

    fix(SA-603): formatting

--- config.tf
+++ config.tf
@@ -60,7 +60,7 @@ resource "aws_config_configuration_recorder" "mgmt_config_recorder" {
   }
 
   recording_mode {
-    recording_frequency     = "CONTINUOUS"
+    recording_frequency = "CONTINUOUS"
   }
 }
 
