4d0fe595f70e69459e4fabadf1041e49c3af6275
Warren Ayling <warren.ayling@appvia.io>
2025-10-10 11:30:21 +0100

    fix(SA-609): formatting

--- locals.tf
+++ locals.tf
@@ -7,5 +7,5 @@ locals {
   tags = merge(var.tags, {})
 
   # using Organization - resolve the mgmt account id (it's only ever the one account in the Management OU)
-  mgmt_account_id       = data.aws_organizations_organization.this.master_account_id
+  mgmt_account_id = data.aws_organizations_organization.this.master_account_id
 }
--- modules/config/locals.tf
+++ modules/config/locals.tf
@@ -8,5 +8,5 @@ locals {
 
   # using Organization - resolve the mgmt account id (it's only ever the one account in the Management OU)
   logarchive_account_id = var.logarchive_account_id
-  organization_id = data.aws_organizations_organization.this.id
+  organization_id       = data.aws_organizations_organization.this.id
 }
