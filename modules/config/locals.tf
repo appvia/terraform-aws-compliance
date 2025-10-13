a611b44c9ebd00c766870f015b5f3f120846c12d
Warren Ayling <warren.ayling@appvia.io>
2025-10-13 10:19:39 +0100

    fix(SA-603): formmating

--- modules/config/locals.tf
+++ modules/config/locals.tf
@@ -5,10 +5,10 @@ locals {
 
   ## Tag applied to all resources
   tags = merge(var.tags, {})
- 
+
   ## used to resovle the target S3 bucket for Config data
   logarchive_account_id = var.logarchive_account_id
 
   # capture the Organization - to then resolve the mgmt account id in locals
-  organization_id       = data.aws_organizations_organization.this.id
+  organization_id = data.aws_organizations_organization.this.id
 }
